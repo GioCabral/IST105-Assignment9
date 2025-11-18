358983a39a1ea455e2142b6d9f905618062e2fcc
GioCabral <giolucacab@outlook.com>
2025-11-17 17:20:29 -0800

    mongo ip

--- assignment9/settings.py
+++ assignment9/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vrj)_&g5r#96u6x008@#5q3v(p#n8=3p7p$*0+*n!)$_@pp59b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
